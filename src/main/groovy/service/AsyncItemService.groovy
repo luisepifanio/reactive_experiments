b7bb38809fc7dafefa5df53d0fde5bc43e2ed19a
Luis G. Epifanio <luis.epifanio@mercadolibre.com>
2016-08-08 12:16:27 -0300

    Completed example with multiple items but single thread processing

--- src/main/groovy/service/AsyncItemService.groovy
+++ src/main/groovy/service/AsyncItemService.groovy
@@ -15,8 +15,8 @@ class AsyncItemService {
      * if available or  uses a thread to fetch the value and
      * caches value onNext to
      */
-    Observable<Item> getById(String id) {
-        String key = '/items/' + id
+    public Observable<Item> getById(String id) {
+        final String key = '/items/' + id
         Item cachedItem = Application.INSTANCE.cache.getIfPresent(key) as Item
 
         if (cachedItem) {
@@ -39,20 +39,22 @@ class AsyncItemService {
         }
     }
 
-    Observable<Item> getByIds(final Collection<String> multipleIds) {
-        Collection<Item> source = multipleIds ?: []
+    public Observable<Item> getByIds(final Collection<String> multipleIds) {
+        log.debug 'AsyncItemService.getByIds'
+
+        Collection<Item> source = new ArrayList<>(multipleIds) ?: []
         if( source.isEmpty() ) return Observable.empty()
 
-        return Observable.from(multipleIds)
+        return Observable.from(source)
                 // It is preferable to run in IO Thread
                 .subscribeOn(Schedulers.io())
-                // flatMap is a nice way to
-                .flatMap({ val -> Observable.just(val) })
+                .flatMap({ val -> Observable.from(ItemService.getFutureItemById(val)) })
+                .doOnNext({ log.debug "WTF is $it on ${Thread.currentThread().name} ::-> ${it.getClass().name}"  })
                 .map({ Map it -> MapperFunctions.mapToItemFunction(it) })
                 .doOnNext({ Item anItem ->
                     // It is better to avoid previous result on its own
                     // on next to avoid side effects
-                    Application.INSTANCE.cache.put(key,anItem)
+                    Application.INSTANCE.cache.put('/items/' + anItem.id ,anItem)
                 }).doOnNext({ Item anItem ->
                     // Debug bussines
                     log.debug "Running on ${Thread.currentThread().name} and returned $anItem"
--- src/test/groovy/service/AsyncItemServiceSpec.groovy
+++ src/test/groovy/service/AsyncItemServiceSpec.groovy
@@ -8,6 +8,7 @@ import rx.observers.TestSubscriber
 import spock.lang.Specification
 
 import static org.assertj.core.api.Assertions.assertThat
+import static org.junit.Assert.assertEquals
 
 @Slf4j
 class AsyncItemServiceSpec extends Specification {
@@ -42,9 +43,9 @@ class AsyncItemServiceSpec extends Specification {
         when: "service is invoked to get and item from it's id"
         Observable<Item> observable = asyncItemService.getById(anItemId)
                 .doOnNext({ Item anItem ->
-                    // Debug bussines
-                    log.debug "Test Running on ${Thread.currentThread().name}"
-                })
+            // Debug bussines
+            log.debug "Test Running on ${Thread.currentThread().name}"
+        })
 
         observable.subscribe(testSubscriber);
 
@@ -61,6 +62,52 @@ class AsyncItemServiceSpec extends Specification {
         assertThat(cachedValue).isNotNull()
 
         testSubscriber.assertValue(cachedValue)
+
+        String observingThread = testSubscriber.getLastSeenThread().getName();
+        log.info "Last seen thread $observingThread"
+
+    }
+
+    def 'Single thread concurrency on multiples threads'() {
+
+        setup:
+        TestSubscriber<String> testSubscriber = new TestSubscriber<>()
+
+        and: 'An existing item id'
+        Collection<String> severalItemsIds = Arrays.asList('MLA623187668', 'MLA622553436')
+
+        when: "service is invoked to get and item from it's id"
+        Observable<Item> observable = asyncItemService.getByIds(severalItemsIds)
+                .doOnNext({ Item anItem ->
+            // Debug bussines
+            log.debug "Test Running on ${Thread.currentThread().name}"
+        })
+
+        observable.subscribe(testSubscriber);
+
+        then:
+
+        testSubscriber.awaitTerminalEvent()
+        log.info 'awaiting Terminal Event'
+
+        testSubscriber.assertNoErrors()
+
+        assertThat(observable).isNotNull()
+
+        assertEquals 'Cached items are not 2', 2, Application.INSTANCE.cache.size()
+
+        Collection<Item> cachedValues = severalItemsIds.collect {
+            Application.INSTANCE.cache.getIfPresent('/items/' + it) as Item
+        }
+
+
+        assertThat(cachedValues)
+                .isNotEmpty()
+                .hasSize(2)
+
+        Item[] values = cachedValues.toArray(new Item[cachedValues.size()]);
+
+        testSubscriber.assertValues(values)
 
         String observingThread = testSubscriber.getLastSeenThread().getName();
         log.info "Last seen thread $observingThread"
